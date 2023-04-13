7b74f64a073a858ca6e5a6912a0f8acd48103c1f
Marius Candoi <96848290+MariusCyber@users.noreply.github.com>
2023-04-13 06:35:16 -0400

    Update scriping.sh

--- scriping.sh
+++ scriping.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# 1) Cititi de la tastatura un string si afisati-l.
-# 2) Calculati si afisati suma a 2 numere.
-# 3) Calculati si afisati suma elementelor unui vector
-# 4) Afisati toate elementele pare dintr-un vector
-# 5) Calculati media artimetica a elementelor impare dintr-un vector
-# 6) Afisati frecventa de apartie a unui nr k dintr-un vector. K citit de la tastatura.
-# 7) Se citeste un vector de cuvinte, sa se afiseze lungimea fiecarui cuvant
-# 8) Se citeste un vector de cuvinte, sa se afiseze cel mai lung cuvant
-# 9) Se citeste un vector de numare si un interval (a,b). Sa se afiseze toate elementele vectorului care apartin intervalului (a,b).
-#10) Se citese de la tastatura numele unui director, sa se creeze directorul respectiv.
-#11) Se citeste de la tastatura numele unui fisier, se se stearga fisierul respectiv doar daca acesta exista. Daca fisierul nu exista sa se afiseze un mesaj de eroare.
-#12) Se citeste un numar n, sa se creeze n conturi de utilizatator.
-#13) Se introduce numele unui utilizator de la tastatura, sa se stearga user-ul respectiv.
-#14) Se citeste de la tastatura un vector de cuvinte, se se scrie cu majuscule toate elementele.
-#15) Se citeste continutul unui fisier. Sa se afiseze lunigimea celei mai lungi linii si sa se afiseze lungimea ei.
 
 function problema_1()
 {
